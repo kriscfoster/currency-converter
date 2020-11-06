84cb4b6aa2ba51676b3f739659641c7b7de91070
kriscfoster <krisfoster@live.ie>
2020-11-06 13:20:41 +0000

    added unit test

--- src/test/java/com/kriscfoster/currencyconverter/exchange/ExchangeServiceTest.java
+++ src/test/java/com/kriscfoster/currencyconverter/exchange/ExchangeServiceTest.java
@@ -1,40 +1,41 @@
 package com.kriscfoster.currencyconverter.exchange;
 
-import com.kriscfoster.currencyconverter.rates.RatesResponse;
 import com.kriscfoster.currencyconverter.rates.RatesService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
-import static com.kriscfoster.currencyconverter.currency.CurrencyEnum.EUR;
-import static com.kriscfoster.currencyconverter.currency.CurrencyEnum.GBP;
+import static com.kriscfoster.currencyconverter.currency.CurrencyEnum.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ExchangeServiceTest {
 
-    RatesService ratesService;
-    RatesResponse ratesResponse;
-    HashMap rates;
+    ExchangeService exchangeService;
 
     @BeforeEach
     public void setup() {
-        this.ratesService = mock(RatesService.class);
-        this.ratesResponse = new RatesResponse();
-        this.rates = new HashMap();
+        RatesService ratesService = mock(RatesService.class);
+        Map<String, Number> rates = new HashMap();
         rates.put(EUR.toString(), 0.846703);
         rates.put(GBP.toString(), 0.765623);
-        ratesResponse.setRates(new HashMap<>());
+        rates.put(USD.toString(), 1);
+        when(ratesService.getRates(any())).thenReturn(rates);
+        this.exchangeService = new ExchangeService(ratesService);
     }
 
     @Test
     public void shouldExchangeEurToGbp() {
-        Date today = new Date();
-        when(ratesService.getRates(today)).thenReturn(rates);
-        ExchangeService exchangeService = new ExchangeService(ratesService);
-        assertEquals(90.42, exchangeService.convert(EUR, GBP, today, 100));
+        assertEquals(90.42, exchangeService.convert(EUR, GBP, new Date(), 100));
+    }
+
+    @Test
+    public void shouldExchangeEurToUsd() {
+        assertEquals(118.11, exchangeService.convert(EUR, USD, new Date(), 100));
     }
 }
